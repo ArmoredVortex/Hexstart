db1228471ffbeef99003ea5d663c9167f406f8c9
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2023-02-24 14:55:38 +0530

    add sick mouse effect

---
+++ script.js
@@ -0,0 +1,12 @@
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
