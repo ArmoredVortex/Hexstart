2846ff88576630cb24bcd45a5f54e635f9bd5bfb
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2023-02-25 11:57:14 +0530

    set css cars and format documents

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 const blob = document.getElementById("blob");
 
-window.onpointermove = ({clientX, clientY}) => { 
-  
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
-}
+window.onpointermove = ({ clientX, clientY }) => {
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
