3150e48307c00428f4f3786355b641c275e99090
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2023-02-26 13:07:41 +0530

    add the ability to toggle rotation

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/* -- Glow effect -- */
-
 const blob = document.getElementById("blob");
 const sideMenu = document.getElementById("sidemenu");
 const settings_button = document.getElementById("settings-button");
 const blur = document.getElementById("blur");
+// const hexagon = document.getElementsByClassName("hexagon");
 
 window.addEventListener("pointermove", ({ clientX, clientY }) => {
   if (!blob_switch.checked) return;
@@ -23,4 +22,18 @@ blur.addEventListener("click", () => sideMenu.classList.remove("active"));
 blob_switch.addEventListener("click", () => {
     blob.style.display = blob_switch.checked ? "block" : "none";
     localStorage.setItem("blob", blob_switch.checked);
+});
+
+rotation_switch.addEventListener("click", () => {
+    if (rotation_switch.checked) {
+        for (let i = 0; i < hexagon.length; i++) {
+            hexagon[i].classList.remove("no-rotation");
+        };
+    } else {
+        for (let i = 0; i < hexagon.length; i++) {
+            hexagon[i].classList.add("no-rotation");
+        };
+    }
+
+    localStorage.setItem("rotation", rotation_switch.checked);
 });
